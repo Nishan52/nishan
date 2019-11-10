ce00ca3ebf7c3f9eb64a0a612e3c8ce521b22095
nishan052 <herculeskhatri@gmail.com>
2019-11-10 15:40:40 -0800

    First commit

---
+++ task.py
@@ -0,0 +1,11 @@
+name=input("Enter your Name")
+age=input("Enter your age")
+print("Enter Two numbers")
+num1=int(input("Enter your first number"))
+num2=int(input("Enter your second number"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quoitent=num1/num2
+print("Hi ",name,"\nYour age is ",age)
+print("The sum is",sum,"\nThe product is",product,"\nThe quoitent is",float(quoitent),"\nThe difference is",difference)
